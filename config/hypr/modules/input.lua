32dcf3cd276bd3f74b624a84612dc5a4b4b85cdd
Dmytro Krynytsyn <dmitry.krinitsyn@gmail.com>
2026-06-30 09:50:33 +0200

    hypr on lua

---
+++ config/hypr/modules/input.lua
@@ -0,0 +1,33 @@
+
+
+---------------
+---- INPUT ----
+---------------
+
+hl.config({
+    input = {
+        kb_layout  = "us",
+        kb_variant = "",
+        kb_model   = "",
+        kb_options = "",
+        kb_rules   = "",
+
+        follow_mouse = 1,
+
+        sensitivity = -0.2, -- -1.0 - 1.0, 0 means no modification.
+
+        touchpad = {
+            natural_scroll = false,
+            disable_while_typing = true,
+            clickfinger_behavior = true,
+        },
+    },
+})
+
+hl.gesture({
+    fingers = 3,
+    direction = "horizontal",
+    action = "workspace"
+})
+
+
---
+++ config/hypr/modules/monitors.lua
@@ -0,0 +1,12 @@
+------------------
+---- MONITORS ----
+------------------
+
+-- See https://wiki.hypr.land/Configuring/Basics/Monitors/
+hl.monitor({
+    output   = "DSI-1",
+    mode     = "1200x1920@50",
+    position = "auto",
+    scale    = "1.8",
+    transform = 3,
+})
