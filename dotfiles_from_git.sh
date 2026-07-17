476d851d664e7885dfb089200dc6cbee12e5abb9
Dmytro Krynytsyn <dmitry.krinitsyn@gmail.com>
2026-07-17 19:37:23 +0200

    more

--- dotfiles_from_git.sh
+++ dotfiles_from_git.sh
@@ -12,5 +12,10 @@ cp -r ./dotfiles/config/nvim/ /home/dmytro/.config/
 cp -r ./dotfiles/config/snappy-switcher/ /home/dmytro/.config/
 cp -r ./dotfiles/config/VSCodium/ /home/dmytro/.config/
 cp ./dotfiles/etc/keyd/default.conf /etc/keyd/default.conf
+cp ./dotfiles/etc/udev/hwdb.d/99-touchpad-fuzz.hwdb /etc/udev/hwdb.d/99-touchpad-fuzz.hwdb
 chown -R dmytro:dmytro /home/dmytro/.config/
 chown dmytro:dmytro /etc/keyd/default.conf
+hyprctl reload
+keyd reload
+systemd-hwdb update
+udevadm trigger
--- dotfiles_to_git.sh
+++ dotfiles_to_git.sh
@@ -8,3 +8,5 @@ mkdir ./dotfiles/config/VSCodium
 mkdir ./dotfiles/config/VSCodium/User
 cp ~/.config/VSCodium/User/* ./dotfiles/config/VSCodium/User
 cp /etc/keyd/default.conf ./dotfiles/etc/keyd
+mkdir -p ./dotfiles/etc/udev/hwdb.d
+cp /etc/udev/hwdb.d/99-touchpad-fuzz.hwdb ./dotfiles/etc/udev/hwdb.d
